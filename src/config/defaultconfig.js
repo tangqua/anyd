5c4d1db2f2f666c31b32865a62b6eb83babc680f
tangqua <373292898@qq.com>
2018-09-21 17:00:33 +0800

    更新

--- src/config/defaultconfig.js
+++ src/config/defaultconfig.js
@@ -1,5 +1,6 @@
 module.exports={
 	root:process.cwd(),
 	port:3000,
-	hostname:"localhost"
+	hostname:"localhost",
+	compress:/\.(html|css|js|md)/
 }
--- src/helper/route.js
+++ src/helper/route.js
@@ -1,21 +1,46 @@
 const fs= require('fs')
+const path=require('path')
+const handlebars=require('handlebars')
 const promisify=require('util').promisify
 const stat=promisify(fs.stat);
 const readdir=promisify(fs.readdir);
+const config=require('../config/defaultconfig')
+const mime=require('./mime')
+const compress=require('./compress')
+
+const tplPath=path.join(__dirname, '../template/dir.tpl')
+const source=fs.readFileSync(tplPath)
+const template=handlebars.compile(source.toString())
 
 module.exports=async function (req,res,filePath) {
 	try {
 		const stats = await stat(filePath)
 		if(stats.isFile()){
+			const contentType=mime(filePath)
 			res.statusCode=200;
-			res.setHeader('Content-Type','text/plain');
-			fs.createReadStream(filePath).pipe(res);
+			res.setHeader('Content-Type',contentType);
+			let rs=fs.createReadStream(filePath)
+			if(filePath.match(config.compress)){
+				rs=compress(rs,req,res) 
+			}
+			rs.pipe(res);
 
 		}else if(stats.isDirectory()){
 			const files=await readdir(filePath)
 			res.statusCode=200;
-			res.setHeader('Content-Type','text/plain');
-			res.end(files.join(','))
+			res.setHeader('Content-Type','text/html');
+			const dir=path.relative(config.root,filePath)
+			const data={
+				title:path.basename(filePath),
+				dir:dir ? `/${dir}` : '',
+				files:files.map(file=>{
+					return {
+						file,
+						icon:mime(file)
+					}
+				})
+			}
+			res.end(template(data))
 		}
 	} catch (err) {
 		
